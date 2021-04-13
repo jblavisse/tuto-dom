d581487d23d4e553c7d92c5940874bbee7ab17b6
Jean-Baptiste Lavisse <jean-baptiste@jblavisse.fr>
2021-04-13 14:20:12 +0200

    Fix prev exercises

--- prev/addEventListener.js
+++ prev/addEventListener.js
@@ -1,14 +1,25 @@
 
 function changeMainTitle() {
+    //Récupère le titre
     let maintitle = document.querySelector("#titleone");
+    // L'affiche dans la console pour voir si bien récupéré
     console.log(maintitle);
+    // Ajoute (ou supprime) au titre la classe maintitle
     maintitle.classList.toggle("maintitle");
 }
 
+// Récupère le bouton qui a pour id enableBadassTitle
 let btn = document.querySelector("#enableBadassTitle");
+// Affiche le bouton dans la console pour voir si bien récupéré
 console.log(btn);
 
+// Ajoute un écouteur d'événement au click qui vient exécuter
+// à chaque fois changeMainTitle
 btn.addEventListener("click", changeMainTitle);
+
+// Rappel
+elem.innerHTML = "Truc"; // Mets "Truc" comme contenu de l'élément elem 
+console.log(elemForm.value); // Affiche la valeur du champ texte
 
 
 function updateTextSpy() {
