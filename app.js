2f5da384fb882257f864dce8d4c886f5ee54b2a1
Jean-Baptiste Lavisse <jean-baptiste@jblavisse.fr>
2021-04-13 10:31:20 +0200

    Add spy exercise

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 /*
-1. Ca va venir questionner le DOM (document) et rechercher 
-le premier élément qui correspond au sélecteur CSS mis en paramètre
-de la fonction querySelector
+1. Vous allez récupérer l'article qui a pour id bestart
+L'afficher dans la console
+*/
+let bestArt = document.querySelector("#bestart");
+console.log(bestArt);
 
-2. Une fois que l'élément a été trouvé (null si pas), celui-ci
-est transféré dans la variable title
+/*
+2. Vous allez récupérer tous les titres qui ont pour classe 
+article__title
+Les afficher dans la console
 */
 
-// querySelector: le premier élément qui correspond
-let title = document.querySelector("#maintitle");
-console.log(title);
+let titles = document.querySelectorAll(".article__title");
+console.log(titles);
 
-// querySelectorAll: tous les éléments qui correspondent
-let paras = document.querySelectorAll(".para");
-console.log(paras);
+// textContent -> Uniquement Le texte d'un élément (si il trouve des balises, il les enlève)
+console.log(titles[0].textContent);
+
+// innerHTML -> L'intégralité de ce qu'il y a dans l'élément
+console.log(titles[0].innerHTML);
+
+// Je veux venir modifier le contenu de mon élément HTML
+// titles[0].innerHTML = "Youhou la famiilllllleee";
+
+
+// Petit exo
+// Ajouter dans l'HTML un paragraphe (id: spy) ayant pour texte "Je suis un espion"
+// Faire en sorte dans mon fichier JS de modifier le texte de ce paragraphe
+// Pour mettre à la place "J'ai un message caché"
